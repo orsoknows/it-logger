82d456d7118a66f03644b50ce96a1af9d3b17a35
Ken Orsini <korsini515@gmail.com>
2022-03-14 13:51:49 -0400

    project complete

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -17,7 +17,7 @@ const initialState = {
 	error: null,
 };
 
-export default (state = initialState, action) => {
+const logReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case GET_LOGS:
 			return {
@@ -43,11 +43,13 @@ export default (state = initialState, action) => {
 				logs: state.logs.map((log) =>
 					log.id === action.payload.id ? action.payload : log
 				),
+				loading: false,
 			};
 		case SEARCH_LOGS:
 			return {
 				...state,
 				logs: action.payload,
+				loading: false,
 			};
 		case SET_CURRENT:
 			return {
@@ -69,8 +71,11 @@ export default (state = initialState, action) => {
 			return {
 				...state,
 				error: action.payload,
+				loading: false,
 			};
 		default:
 			return state;
 	}
 };
+
+export default logReducer;
